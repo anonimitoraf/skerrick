f62bd5f63e248ca7d12aef477bb49366f312ee6e
Rafael Nicdao <nicdaoraf@gmail.com>
2022-01-01 00:46:12 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# JIVE
+**J**avascript **Interactive** de**V**elopment **Environment** (I could've gone for JIDE but doesn't sound as nice and is also not an actual word ;))
+
+REPL-driven development for Javascript in Emacs. Powered by [Replete](https://github.com/jamesdiacono/Replete)
+
+Inspired by (check them out!):
+* SLIME: The Superior Lisp Interaction Mode for Emacs
+* CIDER: The Clojure(Script) Interactive Development Environment that Rocks!
+
+WORK IN-PROGRESS
+
+## Demos
+
+## Configuration
+* `jive--replete-js-path` (required) - Path to replete.js. See https://github.com/jamesdiacono/Replete/blob/trunk/replete.js
+
+## TODO
+* LICENSE
+* Proper popups
+* Support for not just NodeJS
+* etc
---
+++ jive.el
@@ -0,0 +1,80 @@
+;;; jive.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Rafael Nicdao
+;;
+;; Author: Rafael Nicdao <https://github.com/anonimitoraf>
+;; Maintainer: Rafael Nicdao <nicdaoraf@gmail.com>
+;; Created: January 01, 2022
+;; Modified: January 01, 2022
+;; Version: 0.0.1
+;; Keywords: javascript js repl repl-driven
+;; Homepage: https://github.com/anonimitoraf/jive
+;; Package-Requires: ((emacs "27.1") (popup))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; REPL-driven development for Javascript
+;;
+;;  Description
+;;
+;;; Code:
+
+(require 'popup)
+
+(defvar jive--process nil)
+(defvar jive--process-buffer "*jive-process*")
+(defvar jive--replete-js-path nil)
+(defvar jive--region-beginning)
+(defvar jive--region-end)
+
+(defun jive--process-filter (_process output)
+  ;; TODO Exempt the initial messages
+  ;; TODO Remove repetitive code
+  (let* ((stdout+return (when (string-match "<replete_stdout>\\(\\(?:.\\|\n\\)*\\)\n</replete_stdout>\\(.*\\)" output)
+                          (list (match-string-no-properties 1 output)
+                                (match-string-no-properties 2 output))))
+         (stdout (car stdout+return))
+         (stderr+return (when (string-match "<replete_stderr>\\(\\(?:.\\|\n\\)*\\)\n</replete_stderr>\\(.*\\)" output)
+                          (list (match-string-no-properties 1 output)
+                                (match-string-no-properties 2 output))))
+         (stderr (car stderr+return))
+         (return (or (cadr stdout+return) (cadr stdout+return) output)))
+    (with-current-buffer jive--process-buffer
+      (goto-char (point-max))
+      (when stdout (insert stdout ?\n))
+      (when stderr (insert (propertize stderr 'face '(:foreground "red")))))
+    (popup-tip (concat "=> " return) :point jive--region-end)))
+
+(defun jive-start ()
+  "Start JIVE"
+  (interactive)
+  (unless (and jive--process (process-live-p jive--process))
+    (if jive--replete-js-path
+        (user-error "jive--replete-js-path needs to be set!")
+      (setq jive--process (make-process :name "jive"
+                                        :buffer jive--process-buffer
+                                        :filter #'jive--process-filter
+                                        :command '("node" jive--replete-js-path "--experimental-import-meta-resolve" ))))))
+
+(defun jive-stop ()
+  "Stop JIVE"
+  (interactive)
+  (when (and jive--process (process-live-p jive--process))
+    (kill-process jive--process)))
+
+(defun jive-eval-region ()
+  "Evaluate the selected JS code"
+  (interactive)
+  (setq jive--region-beginning (region-beginning)
+        jive--region-end (region-end))
+  (let ((selected-code (format "%s" (buffer-substring-no-properties jive--region-beginning
+                                                                    jive--region-end))))
+    ;; TODO Un-hardcode platform
+    ;; TODO Locator/file path
+    (process-send-string jive--process (concat (json-serialize (list :platform "node"
+                                                                     :source selected-code))
+                                               "\n"))))
+
+(provide 'jive)
+;;; jive.el ends here

4732e2d8faa84feacc518d7519b265bd021b4060
Rafael Nicdao <nicdaoraf@gmail.com>
2022-02-28 18:14:30 +1100

    Emacs: Add LICENSE and link github in Commentary

--- skerrick.el
+++ skerrick.el
@@ -13,10 +13,21 @@
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; Commentary:
-;; REPL-driven development for NodeJS
+;; skerrick is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; skerrick is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with skerrick. If not, see <https://www.gnu.org/licenses/>.
 ;;
-;;  Description
+;;; Commentary:
+;; REPL-driven development for NodeJS. See https://github.com/anonimitoraf/skerrick for more info.
 ;;
 ;;; Code:
 

3900ef76b31fc6f75964fd21252240eecb6e83c7
Rafael Nicdao <nicdaoraf@gmail.com>
2023-11-07 21:57:44 +1100

    Make a start on processing ObjectPatterns

--- server/src/repl/export-named-declaration.ts
+++ server/src/repl/export-named-declaration.ts
@@ -1,8 +1,9 @@
 import * as t from "@babel/types";
 import { NodePath, PluginPass } from "@babel/core";
-import { extractFileName, DEBUG } from "./utils";
+import { extractFileName, DEBUG, unexpected } from "./utils";
 import _ from "lodash";
 import { registerValue, registerExport } from "./state";
+import { Identifier } from "typescript";
 
 export function exportNamedDeclaration(
   path: NodePath<t.ExportNamedDeclaration>,
@@ -27,25 +28,23 @@ export function exportNamedDeclaration(
     return;
   }
 
-  // Process exports that have multiple lines of declarations
-  // See examples below
+  // Process exports that have multiple lines of declarations. See examples below
   const topDeclaration = path.node.declaration;
   if (
     t.isVariableDeclaration(topDeclaration) &&
     topDeclaration.declarations.length > 0
   ) {
     for (const declaration of topDeclaration.declarations) {
+      const id = declaration.id as t.Identifier;
       // E.g.
       // const o = { x: 1, y: 2 };
       // export const { x, y: y1 } = o;
       if (t.isObjectPattern(declaration.id)) {
-        // TODO
-        // OR SHOULD I JUST FIND A WAY TO MAKE IT WORK WITH BINDINGS
+        processObjectPattern(fileName, path, declaration.id);
         continue;
       }
 
       // E.g. export const x = 1, y = 2, z = 3;
-      const id = declaration.id as t.Identifier;
       path.insertAfter([
         registerValue(fileName, id.name, id),
         registerExport(fileName, id.name, id.name),
@@ -58,7 +57,45 @@ export function exportNamedDeclaration(
   return unexpected("Unhandled ExportNamedDeclaration case");
 }
 
-    // TODO When is path.node.declaration undefined?
-    if (path.node.declaration) path.replaceWith(path.node.declaration);
+function processObjectPattern(
+  fileName: string,
+  path: NodePath<t.ExportNamedDeclaration>,
+  objectPattern: t.ObjectPattern
+) {
+  path.traverse({
+    ObjectPattern: (objProp) => {
+      for (const [k] of Object.entries(objProp.scope.bindings)) {
+        DEBUG("transform", "BINDING", k, objProp.scope.hasOwnBinding(k));
+      }
+    },
+  });
+  // DEBUG("transform", "PROPERTIES", objectPattern.properties);
+  for (const prop of objectPattern.properties) {
+    switch (prop.type) {
+      case "ObjectProperty": {
+        // TODO
+        processObjectProperty(fileName, path, prop);
+        break;
+      }
+      case "RestElement": {
+        // TODO
+      }
+      default:
+        return unexpected("Object pattern property type " + prop.type);
+    }
   }
+}
+
+function processObjectProperty(
+  fileName: string,
+  path: NodePath<t.ExportNamedDeclaration>,
+  property: t.ObjectProperty
+) {
+  const key = property.key as t.Identifier;
+  const value = property.value;
+  path.insertAfter(t.stringLiteral("world"));
+  // path.insertAfter([
+  //   registerValue(fileName, key.name, key),
+  //   registerExport(fileName, key.name, id.name),
+  // ]);
 }
